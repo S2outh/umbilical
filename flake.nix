7911e67e4c0ee7ec0057b6e0c1baf37958e67fbc
eleocraft <eliseo.cailloux@gmx.de>
2026-07-31 00:27:58 +0200

    updating rust version

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
+
 {
   description = "embassy flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix/monthly";
-      inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix/monthly";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.fenix.follows = "fenix";
     };
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
-      let 
+      let
         probe-rs-overlay = (final: prev: {
           probe-rs-tools = prev.probe-rs-tools.overrideAttrs {
             cargoBuildFeatures = [ "remote" ];
@@ -21,25 +23,25 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            fenix.overlays.default 
             probe-rs-overlay 
           ];
         };
+        fpkgs = fenix.packages.${system};
+        profile = fpkgs.complete;
+        std-lib = fpkgs.targets.thumbv7em-none-eabihf.latest;
+        rust-analyzer-nightly = fpkgs.rust-analyzer;
+        rust-toolchain = fpkgs.combine [
+          profile.rustc
+          profile.rust-src
+          profile.cargo
+          profile.rustfmt
+          profile.clippy
+          profile.llvm-tools
+          std-lib.rust-std
+        ];
       in
       {
         devShells.default =
-        let
-          profile = pkgs.fenix.complete;
-          std-lib = pkgs.fenix.targets.thumbv7em-none-eabihf.latest;
-          rust-toolchain = pkgs.fenix.combine [
-            profile.rustc-unwrapped
-            profile.rust-src
-            profile.cargo
-            profile.rustfmt
-            profile.clippy
-            std-lib.rust-std
-          ];
-        in
         pkgs.mkShell {
           buildInputs = with pkgs; [
             rust-toolchain
@@ -48,6 +50,8 @@
             # extra cargo tools
             cargo-edit
             cargo-expand
+            cargo-show-asm
+            cargo-binutils
 
             # for flashing
             probe-rs-tools
@@ -55,9 +59,22 @@
 
           # set the rust src for rust_analyzer
           RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
-					# set default defmt log level
-					DEFMT_LOG = "info";
+          # set default defmt log level
+          DEFMT_LOG = "info";
+        };
+
+        packages.default = 
+        (naersk.lib.${system}.override {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        }).buildPackage {
+          src = ./.;
+          FW_VERSION = builtins.getEnv "FW_VERSION";
+          FW_HASH    = builtins.getEnv "FW_HASH";
+
+          DEFMT_LOG = "info";
         };
       }
     );
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(impl_trait_in_assoc_type)] // required by `embassy_executor::task` with the `nightly` feature
 #![feature(const_cmp)]
 #![feature(const_trait_impl)]
 
